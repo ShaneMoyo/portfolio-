d0f11755e069b220c8c94b26ec89daea53100293
Shane Moyo <smoyo@cvent.com>
2020-01-10 10:50:40 -0800

    Add active nav bar.

--- src/navbar.js
+++ src/navbar.js
@@ -15,13 +15,13 @@ function navbar(props) {
           <a class="navbar-item" href="../">
             <p class="title has-text-white large">S. Moyo</p>
           </a>
-          <span class="navbar-burger burger" data-target="navbarMenu" onClick={() => props.isActive(!props.active)}>
+          <span class={props.active ? "navbar-burger  is-active burger" : "navbar-burger  burger"} data-target="navbarMenu" onClick={() => props.isActive(!props.active)}>
               <span></span>
               <span></span>
               <span></span>
           </span>
         </div>
-        <div id="navbarMenu" class="navbar-menu">
+        <div id="navbarMenu" class={props.active ? "navbar-menu is-active animated fadeIn" : "navbar-menu"}>
           <div class="navbar-end">
             <span class="navbar-item">
               <a class="button is-white is-outlined" href="#">
