55548eec2488d0a060e246f945294ab328e06b85
Shane Moyo <smoyo@cvent.com>
2020-01-29 10:35:13 -0800

    Change bio.

--- src/locales/translations.js
+++ src/locales/translations.js
@@ -43,7 +43,7 @@ export const experience = {
         "title": `Software Engineer @ CrowdCompass - Cvent`,
         "body": [
           'Write modern, performant, maintainable code for a diverse array of client and internal projects',
-          'Work with a variety of different languages, platforms, frameworks, and content management systems such as JavaScript, TypeScript, Gatsby, React, Craft, Wordpress, Prismic, and Netlify',
+          'Work with a variety of different languages, platforms, frameworks, and content management systems such as JavaScript, React, Ember, Ruby, Express, and Node',
           'Communicate with multi-disciplinary teams of engineers, designers, producers, and clients on a daily basis'
         ]
       }
